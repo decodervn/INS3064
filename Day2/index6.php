f3e598617363a71d74d6cd9ed93ae96faeac6a33
decodervn <109862173+decodervn@users.noreply.github.com>
2025-09-23 15:34:36 +0700

    Week2

---
+++ Day2/index6.php
@@ -0,0 +1,21 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> This is my first PHP file <h1>
+        <table border="1">
+        <?php
+        for ($i = 0; $i < 5; $i++) {
+            echo "
+            <tr>
+                <td>$i<td/>
+                <tr/>
+                ";
+        }
+        ?>
+    <table>
+</body>
+</html>
